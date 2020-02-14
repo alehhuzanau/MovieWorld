1759016afc7fe087703c8bf7c146145173a36a13
alehhuzanau <aleh.huzanau@gmail.com>
2020-02-14 13:26:02 +0300

    added empty project

---
+++ MovieWorld/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MovieWorld
+//
+//  Created by Aleh Huzanau on 2/14/20.
+//  Copyright © 2020 Clevertec. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MovieWorld/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieWorld
+//
+//  Created by Aleh Huzanau on 2/14/20.
+//  Copyright © 2020 Clevertec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
