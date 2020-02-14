7b7aca259285f8509210b595d94c9b6115d33c22
alehhuzanau <aleh.huzanau@gmail.com>
2020-02-14 16:57:40 +0300

    added the rest subtitles

--- MovieWorld/Controllers/MWMainViewController.swift
+++ MovieWorld/Controllers/MWMainViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SnapKit
 
-typealias Movie = (title: String, year: Int, country: String, genres: [String], rates: [String : Double])
+typealias Movie = (title: String, image: UIImage, year: Int, country: String, genres: [String], rates: [String : String])
 
 class MWMainViewController: UIViewController {
     
@@ -43,9 +43,23 @@ class MWMainViewController: UIViewController {
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: label.font.pointSize)
-        label.text = self.movie?.title
                 
         return label
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        return self.getSubtitleLabel()
+    }()
+    
+    private lazy var genresLabel: UILabel = {
+        let label =  self.getSubtitleLabel()
+        label.textColor = .gray
+
+        return label
+    }()
+    
+    private lazy var ratesLabel: UILabel = {
+        return self.getSubtitleLabel()
     }()
     
     // MARK: - Life cycle funcs
@@ -53,10 +67,7 @@ class MWMainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        self.view.addSubview(self.movieView)
-        self.movieView.addSubview(self.titleLabel)
-        self.movieView.addSubview(self.imageView)
-        
+        self.addSubviews()
         self.makeConstraints()
         
         self.setMovie()
@@ -79,21 +90,52 @@ class MWMainViewController: UIViewController {
             make.top.equalToSuperview().inset(self.subViewEdgeInsets)
             make.left.equalTo(self.imageView.snp.right).inset(self.subViewEdgeInsets)
         }
+        self.subtitleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(self.subViewEdgeInsets.bottom)
+            make.left.equalTo(self.imageView.snp.right).inset(self.subViewEdgeInsets)
+        }
+        self.genresLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.subtitleLabel.snp.bottom).offset(self.subViewEdgeInsets.bottom / 2)
+            make.left.equalTo(self.imageView.snp.right).inset(self.subViewEdgeInsets)
+        }
+        self.ratesLabel.snp.makeConstraints { (make) in
+            make.bottom.equalToSuperview().inset(self.subViewEdgeInsets)
+            make.left.equalTo(self.imageView.snp.right).inset(self.subViewEdgeInsets)
+        }
     }
     
     // MARK: - Others
         
+    private func getSubtitleLabel() -> UILabel {
+        let label = UILabel()
+        label.font = label.font.withSize(13)
+        
+        return label
+    }
+    
+    private func addSubviews() {
+        self.view.addSubview(self.movieView)
+        self.movieView.addSubview(self.imageView)
+        self.movieView.addSubview(self.titleLabel)
+        self.movieView.addSubview(self.subtitleLabel)
+        self.movieView.addSubview(self.genresLabel)
+        self.movieView.addSubview(self.ratesLabel)
+    }
+    
     private func setMovie() {
         self.movie = (title: "Green Book",
+                      image: UIImage(named: Constants.ImageName.movieImage),
                       year: 2018,
                       country: "USA",
                       genres: ["Comedy", "Drama", "Foreign"],
-                      rates: ["IMDB" : 8.2, "KP" : 8.3])
+                      rates: ["IMDB" : "8.2", "KP" : "8.3"]) as? Movie
     }
     
     private func setMovieToViews() {
+        self.imageView.image = self.movie?.image
         self.titleLabel.text = self.movie?.title
-        self.imageView.image = UIImage(named: Constants.ImageName.movieImage)
+        self.subtitleLabel.text = "\(self.movie?.year ?? 0), \(self.movie?.country ?? "")"
+        self.genresLabel.text = self.movie?.genres.joined(separator:",")
+        self.ratesLabel.text = self.movie?.rates.map { $0.0 + " " + $0.1 }.joined(separator: ", ")
     }
 }
-
