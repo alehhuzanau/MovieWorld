d06de8986c90aeaac8528d925b4b884d0cbc017e
alehhuzanau <aleh.huzanau@gmail.com>
2020-03-03 17:26:36 +0300

    added MWNetwork class

---
+++ MovieWorld/Common/Navigation/MWNetwork.swift
@@ -0,0 +1,17 @@
+//
+//  MWNetwork.swift
+//  MovieWorld
+//
+//  Created by Aleh Huzanau on 3/3/20.
+//  Copyright © 2020 Clevertec. All rights reserved.
+//
+
+import UIKit
+
+typealias MWNet = MWNetwork
+
+class MWNetwork {
+    static let sh = MWNetwork()
+    
+    private init() {}
+}
