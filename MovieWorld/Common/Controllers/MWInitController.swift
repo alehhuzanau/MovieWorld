381e3d3415cc5232a454da5bd0b69f5fc26e9949
alehhuzanau <aleh.huzanau@gmail.com>
2020-03-13 18:30:27 +0300

    added others subview to InitController

--- MovieWorld/Common/Controllers/MWInitController.swift
+++ MovieWorld/Common/Controllers/MWInitController.swift
@@ -13,17 +13,43 @@ class MWInitController: UIViewController {
     
     // MARK: - GUI Variables
     
+    private lazy var contentView: UIView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Season".localized
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 34)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: Constants.ImageName.launchImage)
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.masksToBounds = true
         
         return imageView
     }()
     
     private lazy var indicator: UIActivityIndicatorView = {
         let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
         indicator.color = UIColor(named: Constants.ColorName.accentColor)
-        indicator.style = .gray
+        if #available(iOS 13.0, *) {
+            indicator.style = .large
+        } else {
+            indicator.style = .gray
+        }
         
         return indicator
     }()
@@ -54,16 +80,26 @@ class MWInitController: UIViewController {
     }
     
     private func addSubviews() {
-        self.view.addSubview(self.imageView)
+        self.view.addSubview(self.contentView)
+        self.contentView.addSubview(self.titleLabel)
+        self.contentView.addSubview(self.imageView)
         self.view.addSubview(self.indicator)
     }
     
     // MARK: - Constraints
     
     private func makeConstraints() {
-        self.imageView.snp.makeConstraints { (make) in
+        self.contentView.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview()
             make.center.equalToSuperview()
+        }
+        self.titleLabel.snp.makeConstraints { (make) in
+            make.top.left.right.equalToSuperview()
+        }
+        self.imageView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(42)
             make.left.right.equalToSuperview().inset(40)
+            make.bottom.equalToSuperview()
             make.height.equalTo(254)
         }
         self.indicator.snp.makeConstraints { (make) in
